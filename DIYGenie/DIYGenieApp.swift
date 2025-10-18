e57dcaf17b52670f9b255be5647b9ddca85ce5b6
Tye Kowalski <info@diygenieapp.com>
2025-10-18 00:22:16 -0400

    Initial Commit

---
+++ DIYGenie/DIYGenieApp.swift
@@ -0,0 +1,17 @@
+//
+//  DIYGenieApp.swift
+//  DIYGenie
+//
+//  Created by Tye  Kowalski on 10/18/25.
+//
+
+import SwiftUI
+
+@main
+struct DIYGenieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
