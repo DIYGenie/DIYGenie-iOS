2a3948db7e23fc61d08fc01a709517537220b3a2
Tye Kowalski <info@diygenieapp.com>
2025-10-20 12:42:51 -0400

    commit

---
+++ DIYGenie/APIError .swift
@@ -0,0 +1,8 @@
+//
+//  APIError .swift
+//  DIYGenie
+//
+//  Created by Tye  Kowalski on 10/19/25.
+//
+
+import Foundation
---
+++ DIYGenie/DTOs.swift
@@ -0,0 +1,8 @@
+//
+//  DTOs.swift
+//  DIYGenie
+//
+//  Created by Tye  Kowalski on 10/20/25.
+//
+
+import Foundation
