07984fd63e5e8c50634a88aebe9bd7e6043f6b1e
Tye Kowalski <info@diygenieapp.com>
2025-10-22 01:17:09 -0400

    commit

---
+++ DIYGenieApp/DIYGenieApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DIYGenieApp
+//
+//  Created by Tye  Kowalski on 10/22/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
