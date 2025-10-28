e1aa08b64af6672db5bff720397ed211ee34474b
Tye Kowalski <info@diygenieapp.com>
2025-10-28 15:56:35 -0400

    commit

--- DIYGenieApp/Source/Services/SupabaseConfig.swift
+++ DIYGenieApp/Source/Services/SupabaseConfig.swift
@@ -1,8 +1,10 @@
 // SupabaseConfig.swift
 import Foundation
+import Supabase
 
 struct SupabaseConfig {
     static let url = URL(string: Bundle.main.object(forInfoDictionaryKey: "https://qnevigmqyuxfzyczmctc.supabase.co") as? String ?? "")!
     static let anonKey = Bundle.main.object(forInfoDictionaryKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFuZXZpZ21xeXV4Znp5Y3ptY3RjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg5NDc5MjUsImV4cCI6MjA3NDUyMzkyNX0.5wKtzwtNDZt6jjE5gYqNqqWATTdd7g2zVdHB231Z1wQ") as? String ?? ""
+    static let client = SupabaseClient(supabaseURL: url, supabaseKey: anonKey)
 }
 
