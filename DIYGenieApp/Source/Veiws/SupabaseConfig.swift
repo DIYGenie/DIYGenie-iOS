866bdd8c8e4701d7ead0f1e9df91c709c0914f8e
Tye Kowalski <info@diygenieapp.com>
2025-10-28 15:34:58 -0400

    commit

--- DIYGenieApp/Source/Veiws/SupabaseConfig.swift
+++ DIYGenieApp/Source/Veiws/SupabaseConfig.swift
@@ -2,7 +2,7 @@
 import Foundation
 
 struct SupabaseConfig {
-    // Replace these strings with your actual Supabase project URL and anon key.
-    static let supabaseURL = URL(string: "https://qnevigmqyuxfzyczmctc.supabase.co")!
-    static let supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFuZXZpZ21xeXV4Znp5Y3ptY3RjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg5NDc5MjUsImV4cCI6MjA3NDUyMzkyNX0.5wKtzwtNDZt6jjE5gYqNqqWATTdd7g2zVdHB231Z1wQ"
+    static let url = URL(string: Bundle.main.object(forInfoDictionaryKey: "https://qnevigmqyuxfzyczmctc.supabase.co") as? String ?? "")!
+    static let anonKey = Bundle.main.object(forInfoDictionaryKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFuZXZpZ21xeXV4Znp5Y3ptY3RjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg5NDc5MjUsImV4cCI6MjA3NDUyMzkyNX0.5wKtzwtNDZt6jjE5gYqNqqWATTdd7g2zVdHB231Z1wQ") as? String ?? ""
 }
+
