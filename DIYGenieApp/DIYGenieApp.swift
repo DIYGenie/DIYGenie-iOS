936b068a5a5deef952cb8d11326e3c1e71679799
Tye Kowalski <info@diygenieapp.com>
2025-10-30 01:13:02 -0400

    commit

---
+++ DIYGenieApp/DIYGenieApp.swift
@@ -0,0 +1,15 @@
+//
+//  DIYGenieApp.swift
+//
+
+import SwiftUI
+
+@main
+struct DIYGenieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            RootTabs()
+                .tint(.purple)
+        }
+    }
+}
