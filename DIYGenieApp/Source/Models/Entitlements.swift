411886550051df67327353a18533b940dde0c5f5
Tye Kowalski <info@diygenieapp.com>
2025-11-16 13:59:24 -0500

    commit

---
+++ DIYGenieApp/Source/Models/Entitlements.swift
@@ -0,0 +1,8 @@
+//
+//  Entitlements.swift
+//  DIYGenieApp
+//
+//  Created by Tye  Kowalski on 11/16/25.
+//
+
+import Foundation
---
+++ DIYGenieApp/Source/Models/PreviewModels.swift
@@ -0,0 +1,8 @@
+//
+//  PreviewModels.swift
+//  DIYGenieApp
+//
+//  Created by Tye  Kowalski on 11/16/25.
+//
+
+import Foundation
